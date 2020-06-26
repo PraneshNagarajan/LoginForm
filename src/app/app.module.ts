ea458ebf4c7dd53e131538961bacedf057810df4
sathya N <sathya N>
2020-06-26 14:01:20 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { LoginComponent } from './login/login.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { RouterModule } from '../../node_modules/@angular/router';
+import { environment } from '../environments/environment';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule, 
+    FlexLayoutModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTabsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatToolbarModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'signUp', component: SignUpFormComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
